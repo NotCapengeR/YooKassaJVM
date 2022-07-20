f7fbacfc2e8b163cec1a199a17363993a19b1192
rainman-wf <denviva@gmail.com>
2022-07-21 01:57:24 +0300

    currencies enum class

--- src/main/kotlin/yookassa/Main.kt
+++ src/main/kotlin/yookassa/Main.kt
@@ -1,14 +1,18 @@
 package yookassa
 
+import com.google.gson.Gson
+import yookassa.models.Currencies
 import yookassa.models.Payment
 
 fun main() {
 
+    val gson = Gson()
+
     val payment = Payment.Builder()
-        .setAmount(1.0, "RUB")
+        .setAmount(1.0, Currencies.RUB)
         .description("Hueta")
         .recipient("recipient")
         .build()
 
-    println(payment)
+    println(gson.toJson(payment))
 }
