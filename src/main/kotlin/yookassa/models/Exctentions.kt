ba832af9cd9bf1230371d395473d8b150fde970d
NotCapengeR <purple.ka4ok@gmail.com>
2022-07-22 18:54:40 +0500

    Added extension functions to utils package

--- src/main/kotlin/yookassa/models/Exctentions.kt
+++
@@ -1,9 +0,0 @@
-package yookassa.models
-
-import java.text.DecimalFormat
-
-fun Double.toAmount()  : String {
-    if (this < 0) throw RuntimeException("amount can't has negative value")
-    val format = DecimalFormat("#.00")
-    return format.format(this).replace(".", ",")
-}
